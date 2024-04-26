5a96140d322a30508ff3435bcc599e4d27d61b1a
Bendamzi <okunsanyabenjamin40@gmail.com>
2024-04-27 00:19:35 +0100

    My commit

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,20 +5,19 @@ Module: 10-my_github
 Uses Basic Authentication with a personal
 access token to access GitHub API and display user id.
 """
-
-import requests
 import sys
+import requests
+from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-
-    url = f'https://api.github.com/users/{username}'
-    response = requests.get(url, auth=(username, password))
-
-    if response.status_code == 200:
-        user_data = response.json()
-        user_id = user_data.get('id')
-        print(user_id)
-    else:
-        print(None)
+    if len(sys.argv) > 2:
+        username = sys.argv[1]
+        token = sys.argv[2]
+        auth = HTTPBasicAuth(username, token)
+        url = "https://api.github.com/user"
+        response = requests.get(url, auth=auth)
+        if response.status_code == 200:
+            user_data = response.json()
+            print(user_data.get("id"))
+        else:
+            print("None")
