6f7d5c81792556b0f2430c19a35935bae9644db4
Bendamzi <okunsanyabenjamin40@gmail.com>
2024-04-26 17:26:17 +0100

    My commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: 0-hbtn_status
+
+Fetches https://alx-intranet.hbtn.io/status using urllib and displays the body response.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as response:
+        body_response = response.read()
+        print("Body response:")
+        print("\t- type:", type(body_response))
+        print("\t- content:", body_response)
+        print("\t- utf8 content:", body_response.decode('utf-8'))
