4164bf58f24c3af8ddf91639d2b9e2304e6af679
Bendamzi <okunsanyabenjamin40@gmail.com>
2024-04-30 06:25:34 +0100

    My commit

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -s -X OPTIONS -i "$1" | awk '/Allow:/ {print $2}'
+#a Bash script that takes in a URL and displays all HTTP methods the server will accept.
+curl -sI "$1" | grep "Allow" | cut -d " " -f2-
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,13 @@
+#!/usr/bin/python3
+"""Algorithm for finding a peak in a list of unsorted integers."""
+
+
 def find_peak(list_of_integers):
-    if not list_of_integers:
-        return None
-
-    low, high = 0, len(list_of_integers) - 1
-
-    while low < high:
-        mid = (low + high) // 2
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            high = mid
-        else:
-            low = mid + 1
-
-    return list_of_integers[low]
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
+    """
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
